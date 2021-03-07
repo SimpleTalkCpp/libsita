f94c44dbedb02637233f52eb0fbc98a809b22ca9
SimpleTalkCpp <simpletalkcpp@gmail.com>
2021-03-06 18:51:57 -0800

    Project Setup - Linux

--- src/install_ubunta_packages.sh
+++ src/install_ubunta_packages.sh
@@ -1,8 +1,13 @@
-apt install build-essential
-apt install clang g++ cmake
-apt install curl
-apt install zip unzip tar
-apt install libglu1-mesa-dev mesa-utils 
-apt install xorg-dev libx11-dev libwayland-dev
-apt install uuid-dev
-apt install libsdl2-dev
+apt install \
+    build-essential \
+    cmake \
+    curl \
+    zip unzip tar \
+    libglu1-mesa-dev \
+    mesa-utils \
+    xorg-dev \
+    libx11-dev \
+    libwayland-dev \
+    uuid-dev \
+    libsdl2-dev \
+
